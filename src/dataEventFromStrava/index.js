86344039570891ad020678c969c84f251d778aad
Fabio Passos <phabiopassos@gmail.com>
2020-11-24 18:16:35 -0300

    push and quue

--- src/dataEventFromStrava/index.js
+++ src/dataEventFromStrava/index.js
@@ -1,6 +1,27 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS=require('aws-sdk');
+const docClient= new AWS.DynamoDB.DocumentClient({region : 'us-east-2'});
 
-  return {};
+exports.handler = function(event, context, callback) {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+
+  const done = (err, res) => callback(null, {
+    statusCode: err ? '400' : '200',
+    body: err ? err.message : JSON.stringify(res),
+    headers: {
+        'Content-Type': 'application/json',
+    },
+  });
+
+  const body = JSON.parse(event.body);
+  const paramsTable = {
+      TableName : process.env.TABLE_NAME,
+      Item : body
+  };
+
+  if (body.aspect_type == "create"){
+    docClient.put(paramsTable, done);
+  }else {
+    done(new Error(`aspect_type "${event.body.aspect_type}" not supported`));
+  }
+
 };
--- src/verifyStravaWebhook/index.js
+++ src/verifyStravaWebhook/index.js
@@ -1,6 +1,28 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+exports.handler = function(event, context, callback) {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  
+  const done = (err, res) => callback(null, {
+    statusCode: err ? '400' : '200',
+    body: err ? err.message : JSON.stringify(res),
+    headers: {
+        'Content-Type': 'application/json',
+    },
+  });
+  
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;   
+  const queryParams = event.queryStringParameters;
+            
+  let mode = queryParams['hub.mode'];
+  let token = queryParams['hub.verify_token'];
+  let challenge = queryParams['hub.challenge'];
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Verifies that the mode and token sent are valid
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
+      console.log('WEBHOOK_VERIFIED');
+      done(null, {"hub.challenge":challenge});
+    }
+  }
+  done(null, {result:1});
 
-  return {};
 };
